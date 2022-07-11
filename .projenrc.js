217e81208839a6686ae9e328031055e3e90f8287
Marnix Dessing <m.dessing@nijmegen.nl>
2022-07-11 12:39:06 +0200

    feat: use nijemgen projen project type

--- .projenrc.js
+++ .projenrc.js
@@ -1,23 +1,31 @@
-const { awscdk } = require('projen');
-const { JobPermission } = require('projen/lib/github/workflows-model');
+/**
+ * Steps in converting to modules-projen
+ * 1. Include dependency to @gemeentenijmegen/modules-projen in deps
+ * 2. Import project: `const { GemeenteNijmegenCdkApp } = require('@gemeentenijmegen/modules-projen');`
+ * 3. Change project to GemeenteNijmegenCdkApp: `const project = new GemeenteNijmegenCdkApp({`
+ * 4. Remove lint script (now set by default)
+ * 5. Remove depsUpgradeOptions (now set by default)
+ * 6. Remove release (now set by projen default)
+ * 7. Remove license (now set by default)
+ * 8. Remove projenVersion (use projen upgrade workflow)
+ * 9. Keep cdkVersion (this is the minimum cdk version)
+ * 10. Remove gitignore property (now set by default in project type)
+ * 11. Remove workflowBootstapSteps (setup cfn-lint, now handled by project type)
+ * 12. Remove postBuiltSteps (save cf templates, now handled by project type)
+ * 13. Remove Github CloudFormation diff workflow (now handled by project type) 
+ * 14. Add property enableCfnDiffworkflow: true
+ * 15. Add property enableEmergencyProcedure: true
+ * 16. Add property enableCfnLintOnGithub: true
+ */
 
-const project = new awscdk.AwsCdkTypeScriptApp({
-  projenVersion: '0.58.7',
-  cdkVersion: '2.27.0',
-  license: 'EUPL-1.2',
+const { GemeenteNijmegenCdkApp } = require('@gemeentenijmegen/modules-projen');
+const project = new GemeenteNijmegenCdkApp({
+  cdkVersion: '2.27.0', // min cdk version (projen converts this to ^2.27.0 and upgrades it in the package.json with the upgrade task)
   name: 'brp-notificaties',
-  release: true,
   defaultReleaseBranch: 'production',
   majorVersion: 0,
-  depsUpgradeOptions: {
-    workflowOptions: {
-      branches: ['acceptance'],
-    },
-  },
-  scripts: {
-    lint: 'cfn-lint cdk.out/**/*.template.json -i W3005 W2001',
-  },
   deps: [
+    '@gemeentenijmegen/modules-projen',
     'cdk-nag@^2.0.0',
     '@types/aws-lambda',
     'dotenv',
@@ -26,88 +34,6 @@ const project = new awscdk.AwsCdkTypeScriptApp({
     'cdk-remote-stack',
     'aws-sdk',
   ],
-  gitignore: [
-    'test-reports/junit.xml',
-    'test/__snapshots__/*',
-    '.env',
-    '.vscode',
-    '.DS_Store',
-  ],
-  workflowBootstrapSteps: [
-    {
-      name: 'Setup cfn-lint',
-      uses: 'scottbrenner/cfn-lint-action@v2',
-    },
-  ],
-  postBuildSteps: [
-    {
-      name: 'Save CloudFormation templates',
-      run: 'mkdir -p dist && tar -czvf ./dist/cdk.out.tar.gz ./cdk.out',
-    },
-  ],
 });
-
-/**
- * A job to build the base branch and execute a diff on the build cdk.out and base
- * branch cdk.out. A comment is added to the PR indicating if there are differences
- * in the CloudFormation templates.
- */
-const comment = 'between CloudFormation templates on base branch and this branch.';
-project.buildWorkflow.addPostBuildJob('cfn-diff', {
-  permissions: {
-    contents: JobPermission.READ,
-    pullRequests: JobPermission.WRITE,
-  },
-  runsOn: ['ubuntu-latest'],
-  steps: [
-    {
-      name: 'Keep build CloudFormation templates',
-      run: [
-        'tar -xzvf ./dist/cdk.out.tar.gz -C ../',
-        'mv ../cdk.out ../cdk.out.source',
-      ].join(' && '),
-    },
-    {
-      name: 'Checkout',
-      uses: 'actions/checkout@v2',
-      with: {
-        ref: '${{ github.base_ref }}',
-        repository: '${{ github.event.pull_request.head.repo.full_name }}',
-      },
-    },
-    {
-      name: 'Setup cfn-lint',
-      uses: 'scottbrenner/cfn-lint-action@v2',
-    },
-    {
-      name: 'Install dependencies',
-      run: 'yarn install --check-files',
-    },
-    {
-      name: 'Build',
-      run: 'yarn build',
-    },
-    {
-      name: 'Prepare CloudFormation template directories',
-      run: 'mv ../cdk.out.source cdk.out.source && mv cdk.out cdk.out.base',
-    },
-    {
-      name: 'CloudFormation diff',
-      run: [
-        'git diff --no-index --output diff.txt cdk.out.source cdk.out.base || true',
-        'cat diff.txt',
-        '[ -s diff.txt ] && msg="Differences" || msg="No differences"',
-        'echo "Creating a comment on the PR..."',
-        `gh pr comment $PR --body "$(echo $msg) ${comment} \n <details><pre>$(cat diff.txt)</pre></details>" -R $GITHUB_REPOSITORY`,
-      ].join('; '),
-      env: {
-        GITHUB_TOKEN: '${{ secrets.GITHUB_TOKEN }}',
-        GITHUB_REPOSITORY: '${{ github.repository }}',
-        PR: '${{ github.event.pull_request.number }}',
-      },
-    },
-  ],
-});
-
 
 project.synth();
