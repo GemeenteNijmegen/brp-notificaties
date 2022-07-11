59fd3751e68cd1a2afd89e4ea8b36495392bd766
Marnix Dessing <m.dessing@nijmegen.nl>
2022-07-11 17:16:08 +0200

    feat: use development branch for upgrades

--- .projenrc.js
+++ .projenrc.js
@@ -3,6 +3,11 @@ const project = new GemeenteNijmegenCdkApp({
   cdkVersion: '2.27.0', // min cdk version (projen converts this to ^2.27.0 and upgrades it in the package.json with the upgrade task)
   name: 'brp-notificaties',
   defaultReleaseBranch: 'production',
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ['development'], // override from project type
+    },
+  },
   majorVersion: 0,
   deps: [
     '@gemeentenijmegen/modules-projen',
